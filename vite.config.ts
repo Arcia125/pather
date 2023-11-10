39164824002968c0a20d97e6dfa71f84b663e347
Kevin Hallett <kevinmhallett@gmail.com>
2023-11-10 14:00:46 -0600

    vite setup

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.js'),
+      name: 'pather',
+      // the proper extensions will be added
+      fileName: 'pather',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+        },
+      },
+    },
+  },
+  plugins: [dts()],
+})
