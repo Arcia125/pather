e8b6a59b0434dadd6640eabec9f59108d035d2bb
Kevin Hallett <kevinmhallett@gmail.com>
2023-11-12 01:36:38 -0600

    fix errors

--- lib/demo/cellToColor.ts
+++ lib/demo/cellToColor.ts
@@ -1,12 +1,12 @@
 import { CELLS, COLORS } from './constants';
 
 export const cellToColor = (cell: typeof CELLS[keyof typeof CELLS]) => {
-  const cellEntry = Object.entries(CELLS).find(([cellName, cellValue]) => {
-    return cellValue === cell;
+  const cellName = Object.keys(CELLS).find<keyof typeof CELLS>((key) => {
+    return CELLS[key as keyof typeof CELLS] === cell;
   });
-  if (!cellEntry) {
+  if (!cellName) {
     return null;
   }
-  const colorKey = cellEntry[0] as keyof typeof CELLS;
+  const colorKey = cellName;
   return COLORS[colorKey];
 }
