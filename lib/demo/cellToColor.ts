1f60769700eba13be0204a4dfe268e28f433978e
Kevin Hallett <kevinmhallett@gmail.com>
2023-11-12 01:38:08 -0600

    fix typescript types

--- lib/demo/cellToColor.ts
+++ lib/demo/cellToColor.ts
@@ -1,9 +1,9 @@
 import { CELLS, COLORS } from './constants';
 
 export const cellToColor = (cell: typeof CELLS[keyof typeof CELLS]) => {
-  const cellName = Object.keys(CELLS).find<keyof typeof CELLS>((key) => {
+  const cellName = Object.keys(CELLS).find((key) => {
     return CELLS[key as keyof typeof CELLS] === cell;
-  });
+  }) as keyof typeof CELLS;
   if (!cellName) {
     return null;
   }
